01243da284144aad833646e830f7f8f47ca7f83f
Durov Yuriy <u.a.durov@gmail.com>
2021-03-06 13:59:15 +0300

    Refactor PagedQueryuable

--- src/Pagination/Extensions/PaginateExtension.cs
+++ src/Pagination/Extensions/PaginateExtension.cs
@@ -1,28 +1,25 @@
-﻿using BitzArt.Pagination.Interfaces;
-using BitzArt.Pagination.Models;
+﻿using BitzArt.Pagination.Models;
 using System.Linq;
 
 namespace BitzArt.Pagination
 {
     public static class PaginateExtension
     {
-        public static IPagedQueryable<T> Paginate<T>(this IQueryable<T> query, int skip, int take)
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int skip, int take)
         {
             var request = new PageRequest(skip, take);
             return query.Paginate(request);
         }
 
-        public static IPagedQueryable<T> Paginate<T>(this IQueryable<T> query, PageRequest request = null)
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, PageRequest request = null)
         {
             if (query is PagedQueryable<T>) return query as PagedQueryable<T>;
 
             var result = new PagedQueryable<T>();
             result.Query = query;
-            result.UnpaginatedQuery = query;
 
             if (request == null) return result;
             result.PageRequest = request;
-            result.Query = query.Skip(request.Skip).Take(request.Take);
 
             return result;
         }
--- src/Pagination/Models/PagedQueryable.cs
+++ src/Pagination/Models/PagedQueryable.cs
@@ -1,5 +1,4 @@
-﻿using BitzArt.Pagination.Interfaces;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +6,10 @@ using System.Linq.Expressions;
 
 namespace BitzArt.Pagination.Models
 {
-    public class PagedQueryable<T> : IPagedQueryable<T>, IQueryable<T>
+    public class PagedQueryable<T> : IQueryable<T>
     {
         public PageRequest PageRequest { get; set; }
         public IQueryable<T> Query { get; set; }
-        public IQueryable<T> UnpaginatedQuery { get; set; }
 
         public Type ElementType => Query.ElementType;
 
