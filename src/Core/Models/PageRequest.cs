0e5138bb6d5374201b602d0474f7665cf780c7dd
Andrew Shpakovsky <volya93.as@gmail.com>
2022-06-07 16:59:17 +0400

    Added parameterless constructor

--- src/Core/Models/PageRequest.cs
+++ src/Core/Models/PageRequest.cs
@@ -5,6 +5,10 @@
         public virtual int Offset { get; set; }
         public virtual int Limit { get; set; }
 
+        public PageRequest()
+        {
+        }
+
         public PageRequest(int offset = 0, int limit = 100)
         {
             Offset = offset;
