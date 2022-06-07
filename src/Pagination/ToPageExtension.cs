1421b85939f79a1bd94a2062a4c7ffa98153ba24
Andrew Shpakovsky <volya93.as@gmail.com>
2022-06-07 19:27:07 +0400

    Update nuget Packages

--- src/Pagination/ToPageExtension.cs
+++ src/Pagination/ToPageExtension.cs
@@ -13,7 +13,7 @@ namespace System.Linq
 
         public static PageResult<T> ToPage<T>(this IEnumerable<T> query, PageRequest request)
         {
-            var data = query.Skip(request.Skip).Take(request.Take).AsEnumerable();
+            var data = query.Skip(request.Offset).Take(request.Limit).AsEnumerable();
             var total = query.Count();
 
             return new PageResult<T>(data, request, total);
