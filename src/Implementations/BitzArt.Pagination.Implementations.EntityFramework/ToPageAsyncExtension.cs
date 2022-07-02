78ff48ccc89ef3548e0d67c41e9e06448fb52cac
Yuriy Durov <u.a.durov@gmail.com>
2022-07-02 20:13:01 +0400

    Add CancellationToken support

--- src/Implementations/BitzArt.Pagination.Implementations.EntityFramework/ToPageAsyncExtension.cs
+++ src/Implementations/BitzArt.Pagination.Implementations.EntityFramework/ToPageAsyncExtension.cs
@@ -1,18 +1,19 @@
 ﻿using BitzArt.Pagination.Models;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Data.Entity
 {
     public static class ToPageAsyncExtension
     {
-        public static Task<PageResult<T>> ToPageAsync<T>(this IQueryable<T> query, int skip, int take)
-            => query.ToPageAsync(new PageRequest(skip, take));
+        public static Task<PageResult<T>> ToPageAsync<T>(this IQueryable<T> query, int skip, int take, CancellationToken cancellationToken = default)
+            => query.ToPageAsync(new PageRequest(skip, take), cancellationToken);
 
-        public static async Task<PageResult<T>> ToPageAsync<T>(this IQueryable<T> query, PageRequest request)
+        public static async Task<PageResult<T>> ToPageAsync<T>(this IQueryable<T> query, PageRequest request, CancellationToken cancellationToken = default)
         {
-            var data = await query.Skip(request.Offset).Take(request.Limit).ToListAsync();
-            var total = await query.CountAsync();
+            var data = await query.Skip(request.Offset).Take(request.Limit).ToListAsync(cancellationToken);
+            var total = await query.CountAsync(cancellationToken);
 
             return new PageResult<T>(data, request, total);
         }
