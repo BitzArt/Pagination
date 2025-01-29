040a07883244917a71e695cd2fc09d922eee4385
Yuriy Durov <yu.durov@gmail.com>
2025-01-29 20:01:33 +0400

    Use Items property

--- src/BitzArt.Pagination/Extensions/ConvertExtension.cs
+++ src/BitzArt.Pagination/Extensions/ConvertExtension.cs
@@ -7,7 +7,7 @@ public static class ConvertExtension
     /// </summary>
     public static PageResult<TResult> Convert<TSource, TResult>(this PageResult<TSource> source, Func<TSource, TResult> selector)
     {
-        var data = source.Data!.Select(selector);
+        var data = source.Items!.Select(selector);
         return new PageResult<TResult>(data, source.Request, source.Total);
     }
 }
