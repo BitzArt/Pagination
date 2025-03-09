35b418e3aa1e4a5e3c4b1f8c1a4f6877893a6787
Yuriy Durov <u.a.durov@gmail.com>
2025-03-09 17:22:12 +0400

    Update ConvertTests

--- tests/BitzArt.Pagination.Tests/ConvertTests.cs
+++ tests/BitzArt.Pagination.Tests/ConvertTests.cs
@@ -8,14 +8,19 @@ public class ConvertTests
 {
     [Theory]
     [InlineData(10)]
-    public void ToPage_CorrectSource_CorrectResult(int count)
+    [InlineData(100)]
+    public void Convert_OnGoodSource_ShouldReturnCorrectResult(int count)
     {
+        // Arrange
         var sourceData = new List<TestSourceClass>();
         for (int i = 0; i < count; i++) sourceData.Add(new TestSourceClass($"test object {i + 1}"));
 
         var sourcePage = sourceData.ToPage(0, count);
+
+        // Act
         var resultPage = sourcePage.Convert(x => x.ToResult());
 
+        // Assert
         for (int i = 0; i < count; i++)
         {
             var source = sourcePage.Items.ElementAt(i);
